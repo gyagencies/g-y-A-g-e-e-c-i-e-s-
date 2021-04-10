5da42109c347e10772c97ae4b1849c4550b698a2
govardhanreddy111 <>
2021-04-10 20:45:20 +0530

    data structure added

--- src/containers/properties/properties.js
+++ src/containers/properties/properties.js
@@ -2,27 +2,18 @@ import React from 'react';
 import propertiesData from '../../data/properties.json';
 
 function Properties () {
-  const propertyTile = propertiesData.map((property, index) => (
+  const propertyTile = propertiesData.map((property) => (
       <div className="property-tile" key={property.id}>
-        <img src={`../../image_folder/projects/${property.image}`} className="tile-img" />
+        <img src={`image_folder/projects/${property.image}`} className="tile-img" />
           <div className="property-tile-title">{property.title}</div>
           <div className="property-view-more">
-            <a href="javascript:void(0)" className="view-more-btn">View More</a>
+            <div className="view-more-btn">View More</div>
           </div>
       </div>
     ))
   return (
     <div className="property-tiles">
       {propertyTile}
-      {/*{propertiesData.map((property, index) => (*/}
-      {/*  <div className="property-tile">*/}
-      {/*    <img src="image_folder/projects/open_plots/cover_img.jpeg" className="tile-img" />*/}
-      {/*      <div className="property-tile-title">Open Plots</div>*/}
-      {/*      <div className="property-view-more">*/}
-      {/*        <a href="javascript:void(0)" className="view-more-btn">View More</a>*/}
-      {/*      </div>*/}
-      {/*  </div>*/}
-      {/*  ))}*/}
     </div>
   )
 }
